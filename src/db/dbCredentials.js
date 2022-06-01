9272c653d09a1853f6fcfe9781ef064c01eedec3
ssssshel <angelarteaga989@gmail.com>
2022-06-01 18:57:02 -0500

    chn env

--- src/db/dbCredentials.js
+++ src/db/dbCredentials.js
@@ -1,25 +1,17 @@
 const { Pool } = require('pg')
 
+const HOST = process.env.DBHOST
+const USER = process.env.DBUSER
+const PASSWORD = process.env.DBPASSWORD
+const DATABASE = process.env.DBNAME
+const PORT = process.env.DBPORT
+
 const pool = new Pool({
-    host: 'motty.db.elephantsql.com',
-    user: 'rwhmkfpa',
-    password: 'SBJkND-2pjCz_JiMrvLxeJFEonbpBzoI',
-    database: 'rwhmkfpa',
-    port: '5432'
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE,
+    port: PORT
 })
-
-// const HOST = process.env.DBHOST
-// const USER = process.env.DBUSER
-// const PASSWORD = process.env.DBPASSWORD
-// const DATABASE = process.env.DBNAME
-// const PORT = process.env.DBPORT
-
-// const pool = new Pool({
-//     host: HOST,
-//     user: USER,
-//     password: PASSWORD,
-//     database: DATABASE,
-//     port: 5432
-// })
 
 module.exports = pool
